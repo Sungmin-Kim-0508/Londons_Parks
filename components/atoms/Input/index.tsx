d44494a3e598dcc205d2bc97ce7354d6f1cb45b2
Sungmin Kim <seongmin.kim082@gmail.com>
2020-02-22 15:50:23 -0500

    Created Atoms folder

---
+++ components/atoms/Input/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Input(): React.ReactElement {
+  return (
+    <div>
+      <p>Input</p>
+    </div>
+  );
+}
+
+export default Input;
---
+++ components/atoms/Typography/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Typography() {
+  return (
+    <div>
+      <p>Typography</p>
+    </div>
+  );
+}
+
+export default Typography;

2235eb1f6dce3472fc3283f8cdb981857b09320d
jihyeLee4055 <jihyeLee4055@gmail.com>
2020-03-12 12:40:33 -0400

    add Activities

--- components/atoms/Typography/index.tsx
+++ components/atoms/Typography/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import * as Sc from "./style";
 
-function Typography() {
-  return (
-    <div>
-      <p>Typography</p>
-    </div>
-  );
+interface TypographyProps {
+  Title: 'H1';
+  Color: 'black';
+  isBold?: boolean;
+  isTimesNewRoman?: boolean;
 }
+
+const Typography: React.FC<TypographyProps> = ({children, Title, Color, isBold, isTimesNewRoman}) => {
+ return (
+   <Sc.TypographyStyle Title={Title} Color={Color} isBold={isBold} isTimesNewRoman={isTimesNewRoman}>
+      {children}
+   </Sc.TypographyStyle>
+ );
+};
 
 export default Typography;
--- components/atoms/Typography/style.ts
+++ components/atoms/Typography/style.ts
@@ -1,1 +1,17 @@
-import styled from "styled-components";
+import styled, { css, ThemedStyledProps } from "styled-components";
+import { Text } from "react-native"
+
+interface TypographyStyleProps {
+  Title: 'H1';
+  Color: 'black';  
+  isBold?: boolean;
+  isisTimesNewRoman?: boolean;
+
+}
+
+export const TypographyStyle = styled(Text)<TypographyStyleProps>` 
+font-size: ${prop => prop.Title};
+color: ${prop => prop.Color};
+font-weight: ${prop => prop.isBold? 'bold':'normal'};
+font-family: ${prop => prop.isTimesNewRoman? 'Times New Roman':''};
+`
